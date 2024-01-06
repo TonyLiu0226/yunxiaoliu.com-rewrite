16f11ca7982ca5b794470fa7c42308da8a44cc69
TL0226 <TL0226yn@gmail.com>
2024-01-05 22:36:23 -0700

    finish projects

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -1,15 +1,20 @@
 import { Card } from 'flowbite-react';
+import Marquee from "react-fast-marquee";
 
-const ProjectCard = ({Title, Description, Technologies, Link}) => {
+const colors = ['text-gray-900 dark:text-gray-400', 'text-emerald-800 dark:text-amber-300', 'text-blue-700 dark:text-red-500', 'text-fuchsia-800 dark:text-violet-400']
+
+const ProjectCard = ({Title, Description, Technologies, Link, Color}) => {
   return (
-    <Card href={Link} className="!max-w-screen-xl !min-w-80 !w-full transition ease-in-out hover:-translate-y-1 hover:scale-95 hover:dark:bg-emerald-800 hover:bg-amber-300 duration-300">
-      <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-        {Title}
-      </h5>
-      <p className="font-normal text-gray-700 dark:text-gray-400">
-        {Description}
-      </p>
-      <p>Technologies: {Technologies}</p>
+    <Card href={Link} className="!max-w-screen-xl !min-w-72 !w-full transition ease-in-out hover:-translate-y-1 hover:scale-95 hover:dark:bg-emerald-800 hover:bg-amber-300 duration-300">
+        <div className="flex h-full flex-col">
+            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                {Title}
+            </h5>
+            <p className="flex-grow font-normal text-gray-700 dark:text-gray-400">
+                {Description}
+            </p>
+            <Marquee className={colors[Color]} pauseOnHover={true} direction={'right'}>{Technologies}</Marquee>
+        </div>
     </Card>
   );
 }
--- components/Projects.js
+++ components/Projects.js
@@ -4,31 +4,50 @@ import ProjectCard from '../components/ProjectCard'
 const ProjectList = [
   { Title: 'MoodSense',
     Description: 'An AI-enhanced journaling app that performs sentiment analysis on journal entries and comments about your experiences.',
-    Technologies: 'Python, scikit-learn, Pandas, NumPy, TypeScript, NextJS, Firebase',
+    Technologies: ', Python, scikit-learn, Pandas, NumPy, TypeScript, NextJS, Firebase',
     Link: 'https://github.com/TonyLiu0226/next-entries-client/tree/master'},
 
   { Title: 'Dexbooru',
     Description: 'A modernized anime image board for users to share content through posts. Users can like and comment on posts, and can also chat with each other live.',
-    Technologies: 'TypeScript, Sveltekit, Redis, PostgreSQL, TailwindCSS, S3, CloudFront',
+    Technologies: ', TypeScript, Sveltekit, Redis, PostgreSQL, TailwindCSS, S3, CloudFront',
     Link: 'https://github.com/t-shah02/dexbooru-rewrite'},
 
   { Title: 'Epilog',
     Description: 'A seizure tracker app for iOS and Android. Users can log daily seizure activity, record seizures via the camera, and keep track of important events and medications on a built-in calendar.',
-    Technologies: 'TypeScript, React Native, Firebase, NodeJS',
+    Technologies: ', TypeScript, React Native, Firebase, NodeJS',
     Link: 'https://github.com/ubclaunchpad/epilog'},
 
   { Title: 'Restaurant Review Database Project',
     Description: 'A database application that supports adding, modifying, and removing restaurants and menu items, as well as inserting reviews of specific restaurants and menu items by users.',
-    Technologies: 'TypeScript, React Native, Firebase, NodeJS',
+    Technologies: ', JavaScript, HTML, CSS, OracleDB, SQL',
     Link: 'https://github.com/TonyLiu0226/Relational-Databases-Project'},
+
+  {
+    Title: 'Shopinder',
+    Description: 'An app that allows you to make shopping lists ahead of time and set email or text reminders to notify you when its time to shop!',
+    Technologies: ', Svelte, NodeJS, HTML, CSS, Twilio',
+    Link: 'https://devpost.com/software/shopinder'
+  },
+  {
+    Title: 'Unimeetups',
+    Description: 'A meetup app for university students to plan events and make friends with others who are geographically close and share common interests.',
+    Technologies: ', Python, Flask, HTML, CSS, MongoDB',
+    Link: 'https://devpost.com/software/unimeetups'
+  },
+  {
+    Title: 'Tahsin Hasan\'s CPEN 212 Note Transcriber',
+    Description: 'A note transcription device including an automated page-flipping mechanism powered by an ItsyBitsy M4 microcontroller and servo motors. Programmed the device to autonomously operate a connected android camera to take photos of notes and run a machine learning algorithm to transcribe handwritten text. Additionally, implemented a React-based web app to control device functions.',
+    Technologies: ', Python, Flask, CircuitPython, HTML, CSS, React',
+    Link: 'https://github.com/TonyLiu0226/note_transcriber'
+  },
 ]
 const Projects = () => {
     return(
         <div>
         <h1 className={styles.title}>Projects</h1>
-      <div className=" mx-0.5 grid grid-justify-items-center sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4 grid-cols-1 gap-2"> 
+      <div className=" mx-0.5 grid grid-justify-items-center lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 grid-cols-1 gap-2"> 
         {ProjectList.map((project, index) => (
-          <ProjectCard Title={project.Title} Description={project.Description} Technologies={project.Technologies} Link={project.Link} key={index}></ProjectCard>
+          <ProjectCard Title={project.Title} Description={project.Description} Technologies={project.Technologies} Link={project.Link} Color={Math.floor(Math.random() * 4)}key={index}></ProjectCard>
         ))}
     </div>
     </div>
