c7150e22772a6a48ba76a1fad8c334fc93e292c1
TL0226 <TL0226yn@gmail.com>
2024-01-06 23:34:18 -0700

    implements contacts section

--- components/Contacts.js
+++ components/Contacts.js
@@ -1,39 +1,22 @@
 import styles from '../styles/Index.module.css'
 import { Card } from 'flowbite-react';
+import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin, faGithub, faYoutube, faSquareInstagram } from '@fortawesome/free-brands-svg-icons'
 
 const Contacts = () => {
-    return(
+    return( 
         <div>
         <h1 className={styles.title}>Contacts</h1>
       <div className="mx-0.5 w-full flex justify-center items-center"> 
-    <Card className="!max-w-screen-xl !min-w-80 !w-full transition ease-in-out hover:-translate-y-1 hover:scale-105 hover:dark:bg-emerald-800 hover:bg-amber-300 duration-300"
-     renderImage={() => <img width={'40%'} className={styles.aboutImage} src="YL.png" alt="image 1"></img>} horizontal>
-      <h3 className="text-4xl font-bold tracking-tight text-gray-900 dark:text-white">
-        Yunxiao Liu
-      </h3>
-      <p className="font-normal text-gray-900 dark:text-gray-100">
-        Hello, I am a third year Computer Engineering student at the University of British Columbia, focusing mainly on
-        software engineering. I enjoy building meaningful side projects using the full-stack development skills I gained
-        through school and previous work experiences. Some of my more recent projects include MoodSense, an AI-enhanced journaling
-        app, and Dexbooru, a modernized anime image board. Outside of school and work, I enjoy playing video games, watching anime,
-        hiking, skiing, and dining out.
-      </p>
-
-      <p className="font-normal text-gray-900 dark:text-gray-100">
-        Some of the tools and technologies I am familiar with include:
-      </p>
-      <ul>
-        <li className="font-normal text-blue-600 dark:text-rose-400">Languages: JavaScript, TypeScript, Python, Java, C/C++</li>
-        <li className="font-normal text-blue-600 dark:text-rose-400">Frameworks: React, React Native, Sveltekit, NextJS, Flask</li>
-        <li className="font-normal text-blue-600 dark:text-rose-400">Databases: MongoDB, GraphQL, OracleDB, PostgreSQL</li>
-        <li className="font-normal text-blue-600 dark:text-rose-400">Cloud: AWS Lambda, DynamoDB, S3, Cloudfront, ElastiCache</li>
-      </ul>
-
-      <p className="font-normal text-gray-900 dark:text-gray-100">
-        I am currently interested in learning more about AI and machine learning, and am looking to gain more experience with using cloud technologies
-        such as AWS and Docker. I am also seeking internship opportunities for 2024 to further develop my professional career, so any advice, referrals,
-        or job search tips would be welcome!
-      </p>
+    <Card className="!max-w-screen-md !min-w-80 !w-full transition ease-in-out hover:dark:bg-violet-400 hover:bg-fuchsia-800 duration-300">
+      <div className="flex justify-between">
+      <Link href="https://www.linkedin.com/in/yunxiaotonyliu/"><FontAwesomeIcon icon={faLinkedin} size='2x' className="dark:text-white"/></Link>
+      <Link href="https://github.com/TonyLiu0226"><FontAwesomeIcon icon={faGithub} size='2x' className="dark:text-white"/></Link>
+      <Link href="https://www.youtube.com/channel/UCYLZDU2vS-FeL1E2YaWrG3w"><FontAwesomeIcon icon={faYoutube} size='2x' className="dark:text-white"/></Link>
+      <Link href="https://www.instagram.com/tony_26l?igsh=YTQwZjQ0NmI0OA=="><FontAwesomeIcon icon={faSquareInstagram} size='2x' className="dark:text-white"/></Link>
+      </div>
+    <Link href="https://drive.google.com/file/d/1aCyP7vbrY9AkXzAraJyCB_FFTNKGGAL-/view?usp=sharing"><p className="text-emerald-400 dark:text-emerald-100">You can also view my resume here</p></Link>
     </Card>
     </div>
     </div>
